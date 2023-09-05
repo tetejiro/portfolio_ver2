ad7d5a5bc8ef3e049fd1d53d966f283d93889b0d
tetejiro <hy1733505@gmail.com>
2023-09-06 02:29:48 +0900

    input datetime/time ＞タブ移動後、モーダルが表示されない問題を修正

--- resources/js/Pages/Section/Available.vue
+++ resources/js/Pages/Section/Available.vue
@@ -4,24 +4,25 @@ import { onMounted } from "vue";
 
 
 onMounted(() => {
-    initTE({ Input, Timepicker })
+  initTE({ Input, Timepicker })
 
-    let pickerInline_start = document.querySelector("#timepicker-inline-12-start");
-    let pickerInline_end = document.querySelector("#timepicker-inline-12-end");
+  const pickerInline2_start = document.querySelector("#timepicker-inline-24-start");
+  const pickerInline2_end = document.querySelector("#timepicker-inline-24-end");
 
-    const timepickerMaxMin_before = new Timepicker(
-      pickerInline_start, {
-        format12: true,
-        inline: true
-      }
-    );
 
-    const timepickerMaxMin_after = new Timepicker(
-      pickerInline_end, {
-        format12: true,
-        inline: true
-      }
-    );
+  new Timepicker(
+    pickerInline2_start, {
+      format24: true,
+      inline: true
+    }
+  );
+
+  new Timepicker(
+    pickerInline2_end, {
+      format24: true,
+      inline: true
+    }
+  );
 
 });
 
@@ -37,20 +38,18 @@ onMounted(() => {
         <!-- 時間１ -->
         <div
           class="relative"
-          id="timepicker-inline-12-start"
+          id="timepicker-inline-24-start"
           data-te-input-wrapper-init>
           <input
-            name="available_start"
             type="text"
             class="peer block min-h-[auto] w-full rounded border-0 bg-transparent px-3 py-[0.32rem] leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 peer-focus:text-primary data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200 dark:placeholder:text-neutral-200 dark:peer-focus:text-primary [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
-            id="form2" />
+            id="available_start" />
           <label
-            for="form2"
+            for="available_start"
             class="pointer-events-none absolute left-3 top-0 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[1.6] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[0.9rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-data-[te-input-state-active]:-translate-y-[0.9rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-neutral-200 dark:peer-focus:text-primary"
             >Select a time
           </label>
         </div>
-
         <div class="text-md font-medium mb-4 text-gray-900 mx-2">
           ～
         </div>
@@ -58,15 +57,14 @@ onMounted(() => {
         <!-- 時間２ -->
         <div
           class="relative"
-          id="timepicker-inline-12-end"
+          id="timepicker-inline-24-end"
           data-te-input-wrapper-init>
           <input
-            name="available_end"
             type="text"
             class="peer block min-h-[auto] w-full rounded border-0 bg-transparent px-3 py-[0.32rem] leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 peer-focus:text-primary data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200 dark:placeholder:text-neutral-200 dark:peer-focus:text-primary [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
-            id="form2" />
+            id="available_end" />
           <label
-            for="form2"
+            for="available_end"
             class="pointer-events-none absolute left-3 top-0 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[1.6] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[0.9rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-data-[te-input-state-active]:-translate-y-[0.9rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-neutral-200 dark:peer-focus:text-primary"
             >Select a time
           </label>
--- resources/js/Pages/Section/Task.vue
+++ resources/js/Pages/Section/Task.vue
@@ -6,50 +6,73 @@ import { onMounted } from 'vue'
 
 onMounted(() => {
     initTE({ Datetimepicker, Input });
+
+    const pickerTimeOptions_start = document.querySelector('#datetimepicker-timeOptions-start');
+    const pickerTimeOptions_end = document.querySelector('#datetimepicker-timeOptions-end');
+
+    new Datetimepicker(
+        pickerTimeOptions_start, {
+            timepicker: { format24: true },
+        }
+    );
+
+    new Datetimepicker(
+        pickerTimeOptions_end, {
+            timepicker: { format24: true },
+        }
+    );
 });
 
-defineProps(['form'])
+let props = defineProps(['form'])
+
 
 </script>
 
 <template>
     <div class="flex flex-col w-3/5 mx-auto">
 
+        <!-- task content -->
         <h1 class="text-md font-medium mb-4 text-gray-900 mt-10">今はなにをしていますか？</h1>
-        <textarea name="task_content" v-model="form.task_content" class="bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"></textarea>
+        <textarea
+            name="task_content"
+            v-model="props.form.task_content"
+            class="bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out">
+        </textarea>
 
 
         <h1 class="text-md font-medium mb-4 text-gray-900 mt-10">どれくらいかかりそうですか？</h1>
         <div class="flex">
+            <!-- task start -->
             <div
-                class="relative mb-3 w-2/5 mx-2"
-                data-te-date-timepicker-init
-                data-te-input-wrapper-init>
+                class="relative mb-3"
+                data-te-input-wrapper-init
+                id="datetimepicker-timeOptions-start">
                 <input
-                    name="task_start"
-                    v-model="form.task_start"
+                    v-model="props.form.task_start"
                     type="text"
                     class="peer block min-h-[auto] w-full rounded border-0 bg-transparent px-3 py-[0.32rem] leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 peer-focus:text-primary data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200 dark:placeholder:text-neutral-200 dark:peer-focus:text-primary [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
-                    id="form1" />
+                    id="task-start" />
                 <label
-                    for="form1"
+                    for="task-start"
                     class="pointer-events-none absolute left-3 top-0 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[1.6] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[0.9rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-data-[te-input-state-active]:-translate-y-[0.9rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-neutral-200 dark:peer-focus:text-primary"
                     >Select a time
                 </label>
             </div>
+
             <div class="text-md font-medium mb-4 text-gray-900 mx-2">～</div>
+
+            <!-- task end -->
             <div
-                class="relative mb-3 w-2/5 mx-2"
-                data-te-date-timepicker-init
-                data-te-input-wrapper-init>
+                class="relative mb-3"
+                data-te-input-wrapper-init
+                id="datetimepicker-timeOptions-end">
                 <input
-                    name="task_end"
-                    v-model="form.task_end"
+                    v-model="props.form.task_start"
                     type="text"
                     class="peer block min-h-[auto] w-full rounded border-0 bg-transparent px-3 py-[0.32rem] leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 peer-focus:text-primary data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200 dark:placeholder:text-neutral-200 dark:peer-focus:text-primary [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
-                    id="form1" />
+                    id="task-end" />
                 <label
-                    for="form1"
+                    for="task-end"
                     class="pointer-events-none absolute left-3 top-0 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[1.6] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[0.9rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-data-[te-input-state-active]:-translate-y-[0.9rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-neutral-200 dark:peer-focus:text-primary"
                     >Select a time
                 </label>
