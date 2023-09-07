3ae4b57536036d54e8307a1ee63431eeee1192d9
tetejiro <hy1733505@gmail.com>
2023-09-07 23:09:04 +0900

    form の値を各コンポーネントで編集可能に

--- resources/js/Pages/Section/Available.vue
+++ resources/js/Pages/Section/Available.vue
@@ -26,6 +26,9 @@ onMounted(() => {
 
 });
 
+let props = defineProps(['form'])
+
+defineEmits(['change'])
 
 </script>
 
@@ -42,12 +45,15 @@ onMounted(() => {
           data-te-input-wrapper-init>
           <input
             type="text"
+            name="available-start"
+            v-model="props.form.available_start"
+            
             class="peer block min-h-[auto] w-full rounded border-0 bg-transparent px-3 py-[0.32rem] leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 peer-focus:text-primary data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200 dark:placeholder:text-neutral-200 dark:peer-focus:text-primary [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
             id="available_start" />
           <label
             for="available_start"
             class="pointer-events-none absolute left-3 top-0 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[1.6] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[0.9rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-data-[te-input-state-active]:-translate-y-[0.9rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-neutral-200 dark:peer-focus:text-primary"
-            >Select a time
+            >
           </label>
         </div>
         <div class="text-md font-medium mb-4 text-gray-900 mx-2">
@@ -61,17 +67,25 @@ onMounted(() => {
           data-te-input-wrapper-init>
           <input
             type="text"
+            name="available-end"
+            v-model="props.form.available_end"
+            
             class="peer block min-h-[auto] w-full rounded border-0 bg-transparent px-3 py-[0.32rem] leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 peer-focus:text-primary data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200 dark:placeholder:text-neutral-200 dark:peer-focus:text-primary [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
             id="available_end" />
           <label
             for="available_end"
             class="pointer-events-none absolute left-3 top-0 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[1.6] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[0.9rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-data-[te-input-state-active]:-translate-y-[0.9rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-neutral-200 dark:peer-focus:text-primary"
-            >Select a time
+            >
           </label>
         </div>
       </div>
 
       <h1 class="text-md font-medium mb-4 text-gray-900 mt-10">質問するときの注意事項</h1>
-      <textarea name="caution" class="bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"></textarea>
+      <textarea
+        name="caution"
+        v-model="props.form.caution"
+        
+        class="bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out">
+      </textarea>
     </div>
 </template>
--- resources/js/Pages/Section/Task.vue
+++ resources/js/Pages/Section/Task.vue
@@ -3,7 +3,6 @@ import { Datetimepicker, Input, initTE } from "tw-elements";
 import { onMounted } from 'vue'
 
 
-
 onMounted(() => {
     initTE({ Datetimepicker, Input });
 
@@ -25,7 +24,6 @@ onMounted(() => {
 
 let props = defineProps(['form'])
 
-
 </script>
 
 <template>
@@ -38,7 +36,7 @@ let props = defineProps(['form'])
             v-model="props.form.task_content"
             class="bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out">
         </textarea>
-
+        <!-- task content -->
 
         <h1 class="text-md font-medium mb-4 text-gray-900 mt-10">どれくらいかかりそうですか？</h1>
         <div class="flex">
@@ -48,6 +46,7 @@ let props = defineProps(['form'])
                 data-te-input-wrapper-init
                 id="datetimepicker-timeOptions-start">
                 <input
+                    name="task-start"
                     v-model="props.form.task_start"
                     type="text"
                     class="peer block min-h-[auto] w-full rounded border-0 bg-transparent px-3 py-[0.32rem] leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 peer-focus:text-primary data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200 dark:placeholder:text-neutral-200 dark:peer-focus:text-primary [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
@@ -58,6 +57,7 @@ let props = defineProps(['form'])
                     >Select a time
                 </label>
             </div>
+            <!-- task start -->
 
             <div class="text-md font-medium mb-4 text-gray-900 mx-2">～</div>
 
@@ -67,16 +67,18 @@ let props = defineProps(['form'])
                 data-te-input-wrapper-init
                 id="datetimepicker-timeOptions-end">
                 <input
-                    v-model="props.form.task_start"
+                    name="task-end"
+                    v-model="props.form.task_end"
                     type="text"
                     class="peer block min-h-[auto] w-full rounded border-0 bg-transparent px-3 py-[0.32rem] leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 peer-focus:text-primary data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200 dark:placeholder:text-neutral-200 dark:peer-focus:text-primary [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
                     id="task-end" />
                 <label
                     for="task-end"
                     class="pointer-events-none absolute left-3 top-0 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[1.6] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[0.9rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-data-[te-input-state-active]:-translate-y-[0.9rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-neutral-200 dark:peer-focus:text-primary"
-                    >Select a time
+                >
                 </label>
             </div>
+            <!-- task end -->
         </div>
     </div>
 </template>
