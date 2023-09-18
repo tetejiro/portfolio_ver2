b06c31336e2aa3691da713b7b3bd22a548d86789
tetejiro <hy1733505@gmail.com>
2023-09-18 09:34:11 +0900

    周知事項のエラーメッセージ表示・フラッシュメッセージの追加

--- app/Http/Controllers/NoticeInfoController.php
+++ app/Http/Controllers/NoticeInfoController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\NoticeInfoRequest;
 use App\Models\Notice;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
@@ -14,8 +15,15 @@ class NoticeInfoController extends Controller
         $this->middleware('noticeInfo');
     }
 
+    /**
+     * 周知事項 GET ログイン後起動
+     *
+     * @param Request $request
+     * @return void
+     */
     public function index(Request $request)
     {
+        \Log::debug('index');
         $notices = $request->notices;
         $notices = array_reverse($notices->toArray());
 
@@ -24,15 +32,36 @@ class NoticeInfoController extends Controller
         ]);
     }
 
+
+    /**
+     * // TODO バリデーション
+     * 周知事項 POST 保存ボタンから起動
+     *
+     * @param Request $request
+     * @return void
+     */
     public function store(Request $request)
     {
-        Notice::create([
-            'content' => $request->content
+        $request->validate([
+            'content' => 'required|string|max:255'
         ]);
+
+        try {
+            Notice::create([
+                'content' => $request->content
+            ]);
+
+            $message = '登録しました。';
+            $status = 'OK';
+        } catch(\Exception $e) {
+            $message = '登録失敗しました。再入力してください。';
+            $status = 'NG';
+        }
 
         return to_route('dashboard.index')
         ->with([
-            'message' => '登録しました。'
+            'message' => $message,
+            'status' => $status
         ]);
     }
 }
--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -5,7 +5,7 @@ import dayjs from 'dayjs';
 import { reactive } from 'vue'
 import { Head, router } from '@inertiajs/vue3';
 
-let props = defineProps(['notices'])
+let props = defineProps(['notices', 'errors'])
 
 let form = reactive({
     content: null
@@ -32,13 +32,14 @@ let submit = () => {
                         <div class="p-6 text-gray-900">
 
                             <!-- フラッシュメッセージ -->
-                            <div v-if="$page.props.flash.message" class="text-center bg-emerald-200 w-5/12 mx-auto py-1 rounded-md">
+                            <div v-if="$page.props.flash.message"
+                                class="text-center w-5/12 mx-auto py-1 rounded-md"
+                                :class="{'bg-emerald-200': $page.props.flash.status == 'OK',
+                                        'bg-red-500': $page.props.flash.status == 'NG'}">
                                 {{ $page.props.flash.message }}
                             </div>
                             <!-- フラッシュメッセージ -->
-
                             <div class="container px-5 py-8 mx-auto flex h-96">
-
 
                                 <!-- 左側 -->
                                 <div class="overflow-auto hover:overflow-scroll lg:w-5/12 md:w-1/2 rounded-lg p-8 flex flex-col md:ml-auto w-full mt-10 md:mt-0 relative z-10 drop-shadow-xl">
@@ -51,18 +52,23 @@ let submit = () => {
                                 </div>
                                 <!-- 左側 -->
 
-
                                 <!-- 右側 -->
                                 <div class="lg:w-5/12 md:w-1/2 bg-neutral-50 rounded-lg p-8 flex flex-col md:ml-auto w-full mt-10 md:mt-0 relative z-10 drop-shadow-xl">
                                     <h2 class="text-gray-900 text-lg mb-1 font-medium title-font">お知らせ</h2>
                                     <div class="relative mb-4">
                                         <label class="leading-7 text-sm text-gray-600">Message</label>
+
+                                        <!-- エラーメッセージ -->
+                                        <div class="text-red-600" v-if="props.errors.content">
+                                            {{ props.errors.content }}
+                                        </div>
+                                        <!-- エラーメッセージ -->
+
                                         <textarea name="content" v-model="form.content" class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"></textarea>
                                     </div>
                                     <button class="text-white bg-emerald-400 border-0 py-2 px-6 focus:outline-none hover:bg-emerald-300 rounded text-lg">登録</button>
                                 </div>
                                 <!-- 右側 -->
-
 
                             </div>
                         </div>
