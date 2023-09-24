5608c910e1df35fd5c58979615e7e8a10640fa8c
tetejiro <hy1733505@gmail.com>
2023-09-25 02:20:12 +0900

    レイアウト変更（想定人数を多くする）

--- resources/js/Pages/MemberList.vue
+++ resources/js/Pages/MemberList.vue
@@ -1,23 +1,25 @@
 <script setup>
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
-import { Link } from '@inertiajs/vue3'
+import { router } from '@inertiajs/vue3'
 
 let props = defineProps(['members'])
+
+let moveToMyPage = id => {
+  router.get(route('MyPage.show', id ))
+}
 </script>
 <template>
   <AuthenticatedLayout>
 
     <!-- ヘッダー -->
     <template #header>
-      <form @submit.prevent="submit">
-        <header class="text-gray-600 body-font">
-          <div class="container mx-auto flex flex-wrap flex-col md:flex-row items-center">
-            <a class="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
-              <h2 class="font-semibold text-xl text-gray-800 leading-tight">Members</h2>
-            </a>
-          </div>
-        </header>
-      </form>
+      <header class="text-gray-600 body-font">
+        <div class="container mx-auto flex flex-wrap flex-col md:flex-row items-center">
+          <a class="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">Members</h2>
+          </a>
+        </div>
+      </header>
     </template>
     <!-- ヘッダー -->
 
@@ -39,19 +41,18 @@ let props = defineProps(['members'])
 
 
                 <!-- ユーザ -->
-                <div class="flex">
-                  <div v-for="(everyYear, idx) in props.members" :key="everyYear" class="basis-1/6">
-                    <div class="mb-3 text-center" v-if="idx != 6">{{ idx }} 期生</div>
-                    <div class="mb-3 text-center" v-else>達人</div>
-                      <div class="w-full">
-                        <div v-for="member in everyYear" :key="member" class="m-2">
-                          <Link :href="route('my_page', { id: member.id })">
-                            <div class="h-full border-gray-200 border p-4 rounded-lg">
-                              <img alt="team" class="mx-auto w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full" src="https://dummyimage.com/80x80">
-                              <!-- <p class="text-gray-900 text-center">役職名</p> -->
-                              <h2 class="text-center text-gray-700">{{ member.name }}</h2>
-                            </div>
-                          </Link>
+                <div class="flex flex-col justify-center">
+                  <div v-for="(everyYear, idx) in props.members" :key="everyYear">
+                    <div class="flex flex-wrap items-center">
+                      <div class="mb-3 text-center" v-if="idx != 6">{{ idx }} 期生</div>
+                      <div class="mb-3 text-center w-11" v-else>達人</div>
+                      <div v-for="user in everyYear" :key="user" class="m-2">
+                        <div @click="moveToMyPage(user.id)">
+                          <div>
+                            <img alt="team" class="mx-auto w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full" src="https://dummyimage.com/80x80">
+                            <!-- <p class="text-gray-900 text-center">役職名</p> -->
+                            <h2 class="text-center text-gray-700">{{ user.name }}</h2>
+                          </div>
                         </div>
                       </div>
                     </div>
@@ -59,6 +60,8 @@ let props = defineProps(['members'])
                 </div>
                 <!-- ユーザ -->
 
+
+              </div>
             </section>
           </div>
         </div>
