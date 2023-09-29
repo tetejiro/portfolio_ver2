44b49b7de4d56751baf07e27bd09047d607b5483
tetejiro <hy1733505@gmail.com>
2023-09-30 02:57:01 +0900

    エラーページ

---
+++ resources/js/Pages/Error.vue
@@ -0,0 +1,13 @@
+<script setup>
+</script>
+
+<template>
+  <div class="h-screen text-center">
+    <div>
+      <div>エラーが起きました。</div>
+      <div>開発者にエラーを通知しました。</div>
+      <div>お手数をおかけしますが、もう一度やり直してください。</div>
+      <div class="text-blue-800 mx-auto w-fit"><a href="/dashboard">トップページへもどる</a></div>
+    </div>
+  </div>
+</template>
