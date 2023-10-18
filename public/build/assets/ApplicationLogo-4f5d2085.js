72d8b0e9012053f73bd8ef59c01a1cba7fd89629
tetejiro <76621262+tetejiro@users.noreply.github.com>
2023-10-18 23:20:03 +0900

    Delete public/build directory
    Delete public/build directory

--- public/build/assets/ApplicationLogo-4f5d2085.js
+++
@@ -1,1 +0,0 @@
-import{o as c,f as e,u as n}from"./app-541e0e38.js";const t=["src"],i={__name:"ApplicationLogo",setup(r){let o="/favicon.ico";return(s,_)=>(c(),e("img",{src:n(o)},null,8,t))}};export{i as _};
