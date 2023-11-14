b6828aa626f15700c49063d6f69cd2c7a3ad2b76
root <root@ip-172-31-46-160.ap-northeast-1.compute.internal>
2023-11-14 18:02:06 +0000

    マイページレコードのファクトリ修正

--- database/factories/MyPageInfoFactory.php
+++ database/factories/MyPageInfoFactory.php
@@ -16,24 +16,29 @@ class MyPageInfoFactory extends Factory
      */
     public function definition(): array
     {
-        $decade = $this->faker->dateTimeThisDecade;
-        $created_at = $decade->modify('+2 years');
-        $created_at_before = date("Y-m-d H:i:s", strtotime("-1 day", time()));
+	    $task_start = fake()->dateTimeBetween('-2 years', 'now'); //開始時間 
+	    $plusMinute = '+'. (string)rand(0, 14400). ' minute'; //終了時間（ 開始時間にランダムな分数足している）
+	    $start_hour = rand(10, 18); // 空き時間（開始時間）H
+	    $end_hour = rand($start_hour + 1, 19); // 空き時間（終了時間）H
+	    $start_min = rand(0, 59); // 空き時間（開始時間）m
+	    $end_min = rand(0, 59); // 空き時間（終了時間）m
+	    $start_time = $start_hour.':'.str_pad($start_min, 2, 0, STR_PAD_LEFT);
+	    $end_time = $end_hour. ':'. str_pad($end_min, 2, 0, STR_PAD_LEFT);
         $userNum = \App\Models\User::count();
 
         return [
             'user_id' => fake()->numberBetween(1, $userNum),
-            'task_content' => fake()->realText(255),
-            'task_start' => $created_at_before,
-            'task_end' => $created_at,
+            'task_content' => fake()->realText(255, 5),
+            'task_start' => $task_start->format('Y-m-d H:i'),
+            'task_end' => $task_start->modify($plusMinute)->format('Y-m-d H:i'),
             'schedule_status' => fake()->numberBetween(1, 5),
-            'available_start' => '10:00',
-            'available_end' => '15:00',
-            'caution' => fake()->realText(255),
+            'available_start' => $start_time,
+            'available_end' => $end_time,
+            'caution' => fake()->realText(255, 5),
             'strong_point_1' => fake()->realText(),
             'strong_point_2' => fake()->realText(),
             'strong_point_3' => fake()->realText(),
-            'created_at' => $created_at
+            'created_at' => date('Y-m-d H:i')
         ];
     }
 }
